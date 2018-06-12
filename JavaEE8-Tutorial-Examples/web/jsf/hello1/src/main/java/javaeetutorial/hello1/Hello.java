7aaa03fc285794e71c4a206cc4204b1e99ad0c2b
pei <pei.shi@outlook.com>
2018-06-13 00:56:51 +0200

    Update root pom and etc. Made first run of hello1

--- JavaEE8-Tutorial-Examples/web/jsf/hello1/src/main/java/javaeetutorial/hello1/Hello.java
+++ JavaEE8-Tutorial-Examples/web/jsf/hello1/src/main/java/javaeetutorial/hello1/Hello.java
@@ -1,31 +1,20 @@
-/**
- * Copyright (c) 2014 Oracle and/or its affiliates. All rights reserved.
- *
- * You may not modify, use, reproduce, or distribute this software except in
- * compliance with  the terms of the License at:
- * https://github.com/javaee/tutorial-examples/LICENSE.txt
- */
 package javaeetutorial.hello1;
-
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 @Named
 @RequestScoped
+@NoArgsConstructor
+@Setter
+@Getter
 public class Hello {
 
     private String name;
 
-    public Hello() {
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String user_name) {
-        this.name = user_name;
-    }
 }
 
