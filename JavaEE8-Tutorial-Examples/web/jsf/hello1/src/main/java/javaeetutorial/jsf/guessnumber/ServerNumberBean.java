b93f3303901815d2202103ab8288ecce2084c41f
pei <pei.shi@outlook.com>
2018-06-16 14:22:33 +0200

    Merged ajax-guessnumber to hello1

--- JavaEE8-Tutorial-Examples/web/jsf/hello1/src/main/java/javaeetutorial/jsf/guessnumber/ServerNumberBean.java
+++ JavaEE8-Tutorial-Examples/web/jsf/hello1/src/main/java/javaeetutorial/jsf/guessnumber/ServerNumberBean.java
@@ -1,4 +1,4 @@
-package javaeetutorial.ajaxguessnumber;
+package javaeetutorial.jsf.guessnumber;
 
 import java.io.Serializable;
 import java.util.Random;
@@ -17,6 +17,7 @@ public class ServerNumberBean implements Serializable {
     private static final long serialVersionUID = 6575056551121951958L;
     
     private Integer randomInt = null;
+    
     private int min = 0;
     private int max = 10;
 
--- JavaEE8-Tutorial-Examples/web/jsf/hello1/src/main/java/javaeetutorial/jsf/guessnumber/UserNumberBean.java
+++ JavaEE8-Tutorial-Examples/web/jsf/hello1/src/main/java/javaeetutorial/jsf/guessnumber/UserNumberBean.java
@@ -1,8 +1,8 @@
 package javaeetutorial.jsf.guessnumber;
 
 import java.io.Serializable;
-import java.util.Random;
 import javax.enterprise.context.SessionScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
 
 import lombok.Getter;
@@ -14,37 +14,20 @@ public class UserNumberBean implements Serializable {
 
     private static final long serialVersionUID = 5443351151396868724L;
     
-    Integer serverRandomNum = null;
-    
-    String response  = null;
+    @Inject
+    ServerNumberBean serverNumberBean;
     
     @Getter 
     @Setter
     private Integer userInputNumber = null;
     
-    @Getter 
-    @Setter
-    private int min = 0;
-    
-    @Getter 
-    @Setter
-    private int max = 10;
-    
-    
-    public UserNumberBean() {
-        
-        serverRandomNum = Integer.valueOf(new Random().nextInt(max + 1));
-
-        System.out.println("Server random number is: " + serverRandomNum);
-        
-    }
-
+    String response  = null;
     
     public String getResponse() {
         
         response = "User input number " + userInputNumber;
         
-        if ((userInputNumber == null) || (userInputNumber.compareTo(serverRandomNum) != 0)) {
+        if ((userInputNumber == null) || (userInputNumber.compareTo(serverNumberBean.getRandomInt()) != 0)) {
             response += " is NOT EQUAL to server random number.";
         } else {
             response += " is EQUAL to the server random number.";
