1b193e6fbe2ebca0454a0e038a8e0b2a0a69b42a
pei <pei.shi@outlook.com>
2018-06-21 00:40:15 +0200

    Refactored Jsonb

--- JavaEE8-Tutorial-Examples/web/jsonb/jsonbbasics/src/main/java/javaeetutorial/jsonbmodel/person/Person.java
+++ JavaEE8-Tutorial-Examples/web/jsonb/jsonbbasics/src/main/java/javaeetutorial/jsonbmodel/person/Person.java
@@ -6,14 +6,12 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.ToString;
 
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@ToString
 public class Person {
     
     private String name;
