2332c5c769f776a60667f8e4ed8e55113132f2b8
pei <pei.shi@outlook.com>
2018-06-21 00:32:21 +0200

    Refactored Jsonb

--- JavaEE8-Tutorial-Examples/web/jsonb/jsonbbasics/src/main/java/javaeetutorial/jsonbmodel/JsonbBean.java
+++ JavaEE8-Tutorial-Examples/web/jsonb/jsonbbasics/src/main/java/javaeetutorial/jsonbmodel/JsonbBean.java
@@ -1,169 +1,96 @@
-/**
- * Copyright (c) 2017 Oracle and/or its affiliates. All rights reserved.
- *
- * You may not modify, use, reproduce, or distribute this software except in
- * compliance with  the terms of the License at:
- * https://github.com/javaee/tutorial-examples/LICENSE.txt
- */
 package javaeetutorial.jsonbmodel;
 
-import javaeetutorial.jsonbmodel.person.Person;
-import javaeetutorial.jsonbmodel.person.PhoneNumber;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
-import java.io.*;
-import java.util.*;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
 import javax.json.bind.Jsonb;
 import javax.json.bind.JsonbBuilder;
 import javax.json.bind.JsonbConfig;
 
+import javaeetutorial.jsonbmodel.person.Person;
+import javaeetutorial.jsonbmodel.person.PhoneNumber;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
- * This class manages the data from a JSF form, creates Object and serialize this object to JSON.
- * This class also deserialize JSON to object.
- *
- * @author David Kral
+ * This class manages the data from a JSF form, creates Object and serialize
+ * this object to JSON. This class also deserialize JSON to object.
  */
 @Named
 @SessionScoped
+@Data
+@NoArgsConstructor
 public class JsonbBean implements Serializable {
-    
+
     private static final long serialVersionUID = 5427226765445840012L;
-    
-    /* Form properties */
-    protected static final String PHONE_TYPE_HOME = "Home";
-    protected static final String PHONE_TYPE_WORK = "Work";
+
+    /*
+     * The fields pre-set values are defaults, ease testing and demo. 
+     */
+    protected String phoneTypeHome = "Home";
+    protected String phoneTypeWork = "Work";
 
     private String name = "Jason Bourne";
-    private String profession = "Super agent";
+    private String profession = "Machinery Engineer";
 
-    private String phoneType1 = PHONE_TYPE_HOME;
+    private String phoneType1 = phoneTypeHome;
     private String number1 = "123-456-789";
 
-    private String phoneType2 = PHONE_TYPE_WORK;
+    private String phoneType2 = phoneTypeWork;
     private String number2 = "123-555-555";
 
     protected String jsonTextArea = "";
 
-    private Jsonb jsonb;
-    
-    public JsonbBean() {
+    private final Jsonb jsonb = JsonbBuilder.create(new JsonbConfig().withFormatting(true));
 
-        /* JsonbConfig and Jsonb instance creation */
-        JsonbConfig config = new JsonbConfig()
-                .withFormatting(true);
-        this.jsonb = JsonbBuilder.create(config);
-    }
-    
-    /* Getters and setters */
-    public String getPhoneTypeHome() {
-        return PHONE_TYPE_HOME;
-    }
-
-    public String getPhoneTypeWork() {
-        return PHONE_TYPE_WORK;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-    public String getProfession() {
-        return profession;
-    }
-
-    public void setProfession(String profession) {
-        this.profession = profession;
-    }
-
-    public String getPhoneType1() {
-        return phoneType1;
-    }
-
-    public void setPhoneType1(String phoneType1) {
-        this.phoneType1 = phoneType1;
-    }
-
-    public String getNumber1() {
-        return number1;
-    }
-
-    public void setNumber1(String number1) {
-        this.number1 = number1;
-    }
-
-    public String getPhoneType2() {
-        return phoneType2;
-    }
-
-    public void setPhoneType2(String phoneType2) {
-        this.phoneType2 = phoneType2;
-    }
-
-    public String getNumber2() {
-        return number2;
-    }
-
-    public void setNumber2(String number2) {
-        this.number2 = number2;
-    }
-
-    public String getJsonTextArea() {
-        return jsonTextArea;
-    }
-
-    public void setJsonTextArea(String jsonTextArea) {
-        this.jsonTextArea = jsonTextArea;
-    }
-    
     /**
-     *  Action method for the form in index.xhtml.
-     *  Creates Person object and creates formatted JSON
+     * Action method for the form in index.xhtml. Creates Person object and creates
+     * formatted JSON
+     * 
+     * @return is the JSF navigation
      */
     public String createJson() {
         Person person = new Person(this.name, this.profession, new ArrayList<>());
         person.getPhoneNumbers().add(new PhoneNumber(this.phoneType1, this.number1));
         person.getPhoneNumbers().add(new PhoneNumber(this.phoneType2, this.number2));
 
-        /* Serialization to JSON */
+        // Serialization, Obj-->JSON
         this.jsonTextArea = jsonb.toJson(person);
-        
-        /* JSF navigation */
+
         return "jsongenerated";
     }
-    
+
     /**
-     * Action method for form in jsongenerated.xhtml.
-     * Deserialize JSON from the textarea to Person object
-     * and fills these data to form.
+     * Action method for form in jsongenerated.xhtml. Deserialize JSON from the
+     * textarea to Person object and fills these data to form.
+     * 
+     * @return is the JSF navigation
      */
     public String parseJson() {
+
         clearFields();
 
-        /* Deserialization of JSON to Object */
+        // Deserialization, JSON --> Object
         Person person = jsonb.fromJson(this.jsonTextArea, Person.class);
+
         this.name = person.getName();
         this.profession = person.getProfession();
-        if (person.getPhoneNumbers() != null) {
-            int index = 0;
-            for (PhoneNumber number : person.getPhoneNumbers()) {
-                if (index == 0) {
-                    this.number1 = number.getNumber();
-                    this.phoneType1 = number.getType();
-                } else if (index == 1) {
-                    this.number2 = number.getNumber();
-                    this.phoneType2 = number.getType();
-                } else {
-                    break;
-                }
-                index++;
+
+        List<PhoneNumber> phoneNumbers = person.getPhoneNumbers();
+        if (phoneNumbers != null) {
+            if (phoneNumbers.get(0) != null) {
+                this.number1 = phoneNumbers.get(0).getNumber();
+                this.phoneType1 = phoneNumbers.get(0).getType();
+            }
+            if (phoneNumbers.get(1) != null) {
+                this.number2 = phoneNumbers.get(1).getNumber();
+                this.phoneType2 = phoneNumbers.get(1).getType();
             }
         }
 
-        /* JSF navigation */
         return "index";
     }
 
--- JavaEE8-Tutorial-Examples/web/jsonb/jsonbbasics/src/main/java/javaeetutorial/jsonbmodel/person/PhoneNumber.java
+++ JavaEE8-Tutorial-Examples/web/jsonb/jsonbbasics/src/main/java/javaeetutorial/jsonbmodel/person/PhoneNumber.java
@@ -1,42 +1,17 @@
-/**
- * Copyright (c) 2017 Oracle and/or its affiliates. All rights reserved.
- *
- * You may not modify, use, reproduce, or distribute this software except in
- * compliance with  the terms of the License at:
- * https://github.com/javaee/tutorial-examples/LICENSE.txt
- */
 package javaeetutorial.jsonbmodel.person;
 
-/**
- * Phone number of Person
- *
- * @author David Kral
- */
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class PhoneNumber {
+    
     private String type;
     private String number;
-
-    public PhoneNumber() {
-    }
-
-    public PhoneNumber(String type, String number) {
-        this.type = type;
-        this.number = number;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public String getNumber() {
-        return number;
-    }
-
-    public void setNumber(String number) {
-        this.number = number;
-    }
+    
 }
