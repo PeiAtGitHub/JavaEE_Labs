2fea797844ba2270d4323e937b286dde900a75e1
pei <pei.shi@outlook.com>
2018-07-15 01:05:47 +0200

    Moved websocket module 1 level up, and removed web module

---
+++ JavaEE8-Tutorial-Examples/Websocket/etf2/src/main/java/javaeetutorial/websocket/etf2/ETFEndpoint.java
@@ -0,0 +1,49 @@
+package javaeetutorial.websocket.etf2;
+
+import java.io.IOException;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.websocket.OnClose;
+import javax.websocket.OnError;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+
+@ServerEndpoint("/etf")
+public class ETFEndpoint {
+    
+    private static final Logger logger = Logger.getLogger("ETFEndpoint");
+
+    static Queue<Session> sessionsQueue = new ConcurrentLinkedQueue<>();
+    
+    /**
+     * 
+     */
+    public static void send(double price, int volume) {
+        try {
+            for (Session session : sessionsQueue) {
+                session.getBasicRemote().sendText(String.format("%.2f / %d", price, volume));
+            }
+        } catch (IOException e) {
+            logger.log(Level.INFO, e.toString());
+        }
+    }
+
+    @OnOpen
+    public void openConnection(Session session) {
+        sessionsQueue.add(session);
+    }
+    
+    @OnClose
+    public void closedConnection(Session session) {
+        sessionsQueue.remove(session);
+    }
+    
+    @OnError
+    public void error(Session session, Throwable t) {
+        sessionsQueue.remove(session);
+    }
+    
+}
