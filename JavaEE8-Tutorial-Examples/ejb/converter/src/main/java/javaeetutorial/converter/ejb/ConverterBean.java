20192f25726d4a7839d3f24537eaa235c6434924
pei <pei.shi@outlook.com>
2018-07-22 01:07:15 +0200

    Refactored, simplified converter

--- JavaEE8-Tutorial-Examples/ejb/converter/src/main/java/javaeetutorial/converter/ejb/ConverterBean.java
+++ JavaEE8-Tutorial-Examples/ejb/converter/src/main/java/javaeetutorial/converter/ejb/ConverterBean.java
@@ -1,31 +1,22 @@
-/**
- * Copyright (c) 2014 Oracle and/or its affiliates. All rights reserved.
- *
- * You may not modify, use, reproduce, or distribute this software except in
- * compliance with  the terms of the License at:
- * https://github.com/javaee/tutorial-examples/LICENSE.txt
- */
 package javaeetutorial.converter.ejb;
 
 import java.math.BigDecimal;
 import javax.ejb.Stateless;
 
-/**
- * This is the bean class for the ConverterBean enterprise bean.
- * @author ian
- */
 @Stateless
 public class ConverterBean {
-    private final BigDecimal yenRate = new BigDecimal("104.34");
-    private final BigDecimal euroRate = new BigDecimal("0.007");
+    
+    private final static int RESULT_SCALE = 2;
+    
+    private final static BigDecimal JPY_RATE = new BigDecimal("111");
+    private final static BigDecimal EUR_RATE = new BigDecimal("0.85");
     
     public BigDecimal dollarToYen(BigDecimal dollars) {
-        BigDecimal result = dollars.multiply(yenRate);
-        return result.setScale(2, BigDecimal.ROUND_UP);
+        return dollars.multiply(JPY_RATE).setScale(RESULT_SCALE, BigDecimal.ROUND_UP);
     }
     
-    public BigDecimal yenToEuro(BigDecimal yen) {
-        BigDecimal result = yen.multiply(euroRate);
-        return result.setScale(2, BigDecimal.ROUND_UP);
+    public BigDecimal dollarToEuro(BigDecimal dollar) {
+        return dollar.multiply(EUR_RATE).setScale(RESULT_SCALE, BigDecimal.ROUND_UP);
     }
+    
 }
