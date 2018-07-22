4973deac7e215c90e6f6c302c66379630c9e73e9
pei <pei.shi@outlook.com>
2018-07-22 11:27:13 +0200

    Refactored, simplified interceptor

--- JavaEE8-Tutorial-Examples/ejb/interceptor/src/main/java/javaeetutorial/interceptor/ejb/HelloInterceptor.java
+++ JavaEE8-Tutorial-Examples/ejb/interceptor/src/main/java/javaeetutorial/interceptor/ejb/HelloInterceptor.java
@@ -1,38 +1,17 @@
-/**
- * Copyright (c) 2014 Oracle and/or its affiliates. All rights reserved.
- *
- * You may not modify, use, reproduce, or distribute this software except in
- * compliance with  the terms of the License at:
- * https://github.com/javaee/tutorial-examples/LICENSE.txt
- */
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package javaeetutorial.interceptor.ejb;
 
 import java.util.logging.Logger;
 import javax.interceptor.AroundInvoke;
 import javax.interceptor.InvocationContext;
 
-/**
- *
- * @author ian
- */
 public class HelloInterceptor {
-    protected String greeting;
+    
     private static final Logger logger = Logger.getLogger("interceptor.ejb.HelloInterceptor");
-
-    public HelloInterceptor() {
-    }
 
     @AroundInvoke
     public Object modifyGreeting(InvocationContext ctx) throws Exception {
         Object[] parameters = ctx.getParameters();
-        String param = (String) parameters[0];
-        param = param.toLowerCase();
-        parameters[0] = param;
+        parameters[0] = ((String) parameters[0]).toLowerCase();
         ctx.setParameters(parameters);
         try {
             return ctx.proceed();
