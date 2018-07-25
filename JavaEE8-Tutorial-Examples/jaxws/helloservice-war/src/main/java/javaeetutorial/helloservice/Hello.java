6b11c69511e31aa1a05c08bc0ebcf307a062c6ee
pei <pei.shi@outlook.com>
2018-07-26 00:58:36 +0200

    FIXED BUG and simplified project jaxws

--- JavaEE8-Tutorial-Examples/jaxws/helloservice-war/src/main/java/javaeetutorial/helloservice/Hello.java
+++ JavaEE8-Tutorial-Examples/jaxws/helloservice-war/src/main/java/javaeetutorial/helloservice/Hello.java
@@ -1,10 +1,3 @@
-/**
- * Copyright (c) 2014 Oracle and/or its affiliates. All rights reserved.
- *
- * You may not modify, use, reproduce, or distribute this software except in
- * compliance with the terms of the License at:
- * https://github.com/javaee/tutorial-examples/LICENSE.txt
- */
 package javaeetutorial.helloservice;
 
 import javax.jws.WebService;
@@ -13,13 +6,8 @@ import javax.jws.WebMethod;
 @WebService
 public class Hello {
 
-    private final String message = "Hello, ";
-
-    public Hello() {
-    }
-
     @WebMethod
     public String sayHello(String name) {
-        return message + name + ".";
+        return "Hello, " + name + ".";
     }
 }
